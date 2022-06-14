586b06b5de718ca561fdd7c0d8a31c828abafff2
Phuong Anh Hoang <71182218+hpa153@users.noreply.github.com>
2022-06-14 16:09:26 -0400

    fixed home <a> tag

---
+++ build.sh
@@ -0,0 +1,3 @@
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
+cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
